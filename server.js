93762df3c445221c25b91474dd33142852855ac9
Tamal Mondal <tamal.mondal@appsbee.com>
2018-09-20 21:54:39 +0530

    demo

--- server.js
+++ server.js
@@ -1,21 +1,44 @@
-const express = require('express'),
-    app = express(),
-    morgan = require('morgan');
+const port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+const ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const path = require('path');
 
 
-app.engine('html', require('ejs').renderFile);
-app.use(morgan('combined'))
 
-const port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+app.use(morgan('combined'));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// set path for static assets
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get('/', function (req, res) {
-    res.render('./index.html');
+    res.render('index');
+});
+
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error', { status: err.status, message: err.message });
 });
 
 
 app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
 
-module.exports = app ;
+module.exports = app;
